ff2989cbd2070f00f17a950d6ac6919c24323f16
chrisz1337 <143567169+chrisz1337@users.noreply.github.com>
2023-08-29 20:34:19 -0500

    ejercicio4.java

---
+++ ejercicio4.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main
+{
+	public static void main(String[] args) {
+		int numeroA, numeroB;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese un numero A: ");
+        numeroA = sc.nextInt();
+        
+        System.out.println("Ingrese un numero B: ");
+        numeroB = sc.nextInt();
+
+        if(numeroA > 0 && numeroB > 0)
+	        System.out.println("true");
+	   else if(numeroA < 0 && numeroB < 0)
+	        System.out.println("true");
+	}
+}
